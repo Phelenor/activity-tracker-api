28e6a6af768e43b2c45b44679b51a1b77eede4ee
Rafael Boban <rafael.boban00@gmail.com>
2024-04-23 18:41:50 +0200

    Remove jwt test routes

--- app/setup.go
+++ app/setup.go
@@ -27,17 +27,9 @@ func startFiberServer(userRepository storage.UserRepository) {
 
 	controllers.SetupAuthController(app, userRepository)
 
-	app.Get("/unrestricted", func(c *fiber.Ctx) error {
-		return c.SendString("hello world unrestricted")
-	})
-
 	app.Use(jwtware.New(jwtware.Config{
 		SigningKey: jwtware.SigningKey{Key: []byte(os.Getenv("JWT_SECRET"))},
 	}))
-
-	app.Get("/restricted", func(c *fiber.Ctx) error {
-		return c.SendString("hello world restricted")
-	})
 
 	if err := app.Listen(":" + os.Getenv("API_PORT")); err != nil {
 		log.Fatal(err)
